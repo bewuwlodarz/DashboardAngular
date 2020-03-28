5e08b026a93c5dba5c79ab9452f994716b78402d
Bartłomiej Włodarz <bartek.wlodarz@gmail.com>
2020-03-28 14:02:38 +0100

    health status cards

--- DashAngular/src/app/sections/section-health/section-health.component.ts
+++ DashAngular/src/app/sections/section-health/section-health.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Server} from '../../shared/server';
+const SAMPLE_SERVERS = [
+  {id:1, name: 'dev-web', isOnline: true},
+  {id:2, name: 'dev-mail', isOnline: false},
+  {id:3, name: 'dev-web2', isOnline: true},
+  {id:4, name: 'dev-mail2', isOnline: true}
+];
 @Component({
+
+ 
   selector: 'app-section-health',
   templateUrl: './section-health.component.html',
   styleUrls: ['./section-health.component.css']
@@ -8,6 +16,7 @@ import { Component, OnInit } from '@angular/core';
 export class SectionHealthComponent implements OnInit {
 
   constructor() { }
+  servers: Server[] = SAMPLE_SERVERS;
 
   ngOnInit(): void {
   }
