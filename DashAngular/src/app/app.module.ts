c8ed849931d75e1d12e3bf4e5a98fe0c6ff8fd1d
Bartłomiej Włodarz <bartek.wlodarz@gmail.com>
2020-03-28 11:40:18 +0100

    Bar chart

--- DashAngular/src/app/app.module.ts
+++ DashAngular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import {RouterModule} from '@angular/router';
 
 
 import { NgModule } from '@angular/core';
+import {ChartsModule} from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -11,6 +12,9 @@ import { SectionSalesComponent } from './sections/section-sales/section-sales.co
 import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
 import { SectionHealthComponent } from './sections/section-health/section-health.component';
 import { appRoutes } from 'src/routes';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +23,15 @@ import { appRoutes } from 'src/routes';
     SidebarComponent,
     SectionSalesComponent,
     SectionOrdersComponent,
-    SectionHealthComponent
+    SectionHealthComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
